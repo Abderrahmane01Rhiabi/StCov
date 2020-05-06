c94728dd3bc1ac06f404569f649af2a8a31b6320
Abderrahman Rhiabi <rhiabi.abderrahmane.com>
2020-05-06 18:45:40 +0000

    third

---
+++ app/src/main/java/com/example/stcov/ProfileAdmin.java
@@ -0,0 +1,13 @@
+package com.example.stcov;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfileAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_admin);
+    }
+}
---
+++ app/src/main/java/com/example/stcov/SettingsFragment.java
@@ -0,0 +1,6 @@
+package com.example.stcov;
+
+import androidx.fragment.app.Fragment;
+
+public class SettingsFragment extends Fragment {
+}
